de421053eda113242fceb0e5c6452ff52d0f6393
JerfiAliUcar <jerfiucar@icloud.com>
2025-11-15 22:33:26 +0300

    Proje temel MVC dosyaları eklendi. BUS-2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bus_Reservation_Ticketing_Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
