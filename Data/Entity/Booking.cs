d4abd35d727b981c582196bb85a57e38d4e97019
JerfiAliUcar <jerfiucar@icloud.com>
2025-11-19 22:25:21 +0300

    Bus-5 EF Core Entegrasyonu yapıldı, Entityler oluşturuldu ve DB Fİrst ile DB ayağa kaldırıldı

---
+++ Data/Entity/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bus_Reservation_Ticketing_Website.Data.Entity;
+
+public partial class Booking
+{
+    public int BookingId { get; set; }
+
+    public string? UserId { get; set; }
+
+    public DateTime? BookingDate { get; set; }
+
+    public string Pnr { get; set; } = null!;
+
+    public decimal TotalAmount { get; set; }
+
+    public string? BookingStatus { get; set; }
+
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
---
+++ Data/Entity/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bus_Reservation_Ticketing_Website.Data.Entity;
+
+public partial class Schedule
+{
+    public int ScheduleId { get; set; }
+
+    public int RouteId { get; set; }
+
+    public int BusId { get; set; }
+
+    public DateTime DepartureTime { get; set; }
+
+    public decimal Price { get; set; }
+
+    public virtual Bus Bus { get; set; } = null!;
+
+    public virtual Route Route { get; set; } = null!;
+
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
