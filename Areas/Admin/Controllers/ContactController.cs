0e5e391b59b121f53ba712638c2e0cf921a2c61c
Jerfi Ali Uçar <jerfiucar@outlook.com>
2025-11-30 13:30:24 +0300

    BUS-39 Admin Panel Dashboard sayfası tamamlandı

--- Areas/Admin/Controllers/ContactController.cs
+++ Areas/Admin/Controllers/ContactController.cs
@@ -28,5 +28,15 @@ namespace Bus_Reservation_Ticketing_Website.Areas.Admin.Controllers
             }
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> MarkAsRead(int id)
+        {
+            var message = await _dbContext.ContactMessages.FindAsync(id);
+            if (message != null)
+            {
+                message.IsRead = true;
+                await _dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ Migrations/20251129104517_LastChanges.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bus_Reservation_Ticketing_Website.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Buses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Buses",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Buses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Buses",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
